e1b105861b0c049d2a559e4f5df207ad0adbc984
Daniel Samuel <samueldaniel4198@gmail.com>
2025-05-20 20:01:44 +0100

    added the footer section to the page

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 "use strict";
 
 import "tailwindcss/tailwind.css";
+
+//code to get year
+const year = new Date().getFullYear();
+const html = document.querySelector(".fullYear");
+html.textContent = year;
