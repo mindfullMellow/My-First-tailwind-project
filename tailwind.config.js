e22c3b878ec7207ffc6dce7c677da17f1a9a3b7d
Daniel Samuel <samueldaniel4198@gmail.com>
2025-05-20 21:14:14 +0100

    finilaized the website

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,18 @@ const colors = require("tailwindcss/colors");
 export default {
   content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    // overwrite tailwind classes
     extend: {
+      //add on to tailwind classes
       colors: {
         orange: colors.orange,
         teal: colors.teal,
         "light-blue": colors.lightBlue,
       },
+      fontFamily: {
+        fun: ["Varela Round", "sans-serif"],
+      },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
